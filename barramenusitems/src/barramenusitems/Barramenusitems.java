e769cda842d566eb02c03b9f4694082a04ff6565
DIAMANTE <DIAMANTE@DIAMANTE>
2013-03-20 10:01:13 -0500

    new 20/03/13

---
+++ barramenusitems/src/barramenusitems/Barramenusitems.java
@@ -0,0 +1,41 @@
+
+package barramenusitems;
+
+import javax.swing.*;
+import java.awt.*;
+public class Barramenusitems extends JFrame {
+
+    
+    public Barramenusitems () {
+        
+        super ("barra de menus");
+        setSize(500,550);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        
+        JMenu m1 = new JMenu ();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+  
+    public static void main(String[] args) {
+       
+        
+        
+        
+        
+        
+        
+    }
+}
